49ae193fecac405c1c088055f9c7c61e756f6c6f
MikleBe <mikle.pastorbe@gmail.com>
2019-03-12 20:56:46 +0300

    good work

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    // your implementation
+    let mul = findMultipliersOfBase(base);
+    let answer = 0;
+    for (let p in mul) {
+        let zeroes = Math.floor(findPowerOfP(number, p)/mul[p]);
+        if (answer == 0) {
+            answer = zeroes;
+        } else if (answer > zeroes){
+            answer = zeroes;
+        }
+    }
+
+    return answer;
+}
+
+function findPowerOfP(N, p)
+{
+    let count = 0;
+    let r = p;
+    while (r <= N) {
+        let res = Math.floor(N/r);
+        count += res;
+        r = r * p;
+    }
+    return count;
+}
+
+function findMultipliersOfBase(Base)
+{
+    let f = {};
+    for (let i = 2; Base != 1; i++) {
+        if (Base % i == 0) {
+            let count = 0;
+            while (Base % i == 0) {
+                Base = Base / i;
+                count++;
+            }
+            f[i] = count;
+        }
+    }
+    return f;
 }
